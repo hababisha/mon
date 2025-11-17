4ff97d82bfb945fd533aa8850018614fcd273554
hababisha <haileamlakderbie1@gmail.com>
2025-11-17 14:01:08 +0300

    feat: more keywords

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -7,15 +7,18 @@ import (
 
 func TestNextToken(t *testing.T){
 	input := `let five = 5;
-			let ten = 10;
-			let add = fn(x, y) {
-			x + y;
-			};
-			let result = add(five, ten);
-			!-/*5;
-			5 < 10 > 5;
-
-	`
+				let ten = 10;
+				let add = fn(x, y) {
+				x + y;
+				};
+				let result = add(five, ten);
+				!-/*5;
+				5 < 10 > 5;
+				if (5 < 10) {
+				return true;
+				} else {
+				return false;
+				}`
 
 	tests := []struct{
 		expectedType token.TokenType
@@ -41,7 +44,7 @@ func TestNextToken(t *testing.T){
 		{token.IDENT, "y"},
 		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
-		
+
 	}
 
 	l := New(input)
