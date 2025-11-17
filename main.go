1c16702e8f63b631e9516330cb2f91ce70568b0e
hababisha <haileamlakderbie1@gmail.com>
2025-11-17 14:15:29 +0300

    feat: REPL added

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+	"github.com/hababisha/mon/repl"
+)
 
-func main(){
-	fmt.Println("hello world")
+func main() {
+	user, err := user.Current()
+	if err != nil {
+	panic(err)
+	}
+	fmt.Printf("Hello %s! This is the mon programming language!\n",
+	user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
